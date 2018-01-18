7ee286aa299bc4ece7c11cc9b81e50926cab6495
Leonardo Montenegro <leonardomontenegro123@gmail.com>
2018-01-18 16:33:55 -0300

    Added plain ts test case

---
+++ src/test/ts/TicTacToe.test.ts
@@ -0,0 +1,46 @@
+import * as ttt from '../../main/ts/game/TicTacToe'
+
+describe('TicTacToe', () => {
+
+    it('should work for base case', () => {
+        expect(ttt.calculateWinner(
+            [])).toBe(
+            undefined);
+    });
+
+    it('should work for fake X winner case', () => {
+        expect(ttt.calculateWinner(
+            [
+                'X','X','X',
+                'X','X','X',
+                'X','X','X'
+            ])).toBe(
+            'X');
+
+        expect(ttt.calculateWinner(
+            [
+                'q','q','q'
+            ])).toBe(
+            'q');
+    });
+
+    it('should work for real no winner case', () => {
+        expect(ttt.calculateWinner(
+            [
+                'X','X','O',
+                'O','O','X',
+                'X','O','X'
+            ])).toBe(
+            undefined);
+    });
+
+    it('should work for real winner case', () => {
+        expect(ttt.calculateWinner(
+            [
+                'O','X',undefined,
+                undefined,'O','X',
+                'X',undefined,'O'
+            ])).toBe(
+            'O');
+    });
+});
