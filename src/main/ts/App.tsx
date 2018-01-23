5e7a3253dc522b52d40d68629580e4bcc0fa56ba
Leonardo Montenegro <leonardomontenegro123@gmail.com>
2018-01-23 10:25:51 -0300

    Separated styles

--- src/main/ts/App.tsx
+++ src/main/ts/App.tsx
@@ -1,5 +1,17 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+
+//import * as React from 'react';
+//import * as ReactDOM from 'react-dom';
+//import './index.css';
+
+//import {Game} from "./main/ts/react/components/Game";
+import {styles} from "./styles/ticTacToe";
+
+// ReactDOM.render(
+//     <Game />,
+//     document.getElementById('root')
+// );
 
 export default class App extends React.Component {
   render() {
@@ -10,12 +22,3 @@ export default class App extends React.Component {
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
